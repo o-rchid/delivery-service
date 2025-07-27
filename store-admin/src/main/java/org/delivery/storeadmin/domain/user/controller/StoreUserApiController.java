20c09e940cf65b94c564a2923143651929c3d1c3
o-rchid <ozzve35@gmail.com>
2025-07-27 23:49:59 +0900

    사용자 정보 확장

---
+++ store-admin/src/main/java/org/delivery/storeadmin/domain/user/controller/StoreUserApiController.java
@@ -0,0 +1,27 @@
+package org.delivery.storeadmin.domain.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.delivery.storeadmin.domain.user.authorization.model.UserSession;
+import org.delivery.storeadmin.domain.user.controller.model.StoreUserResponse;
+import org.delivery.storeadmin.domain.user.converter.StoreUserConverter;
+import org.delivery.storeadmin.domain.user.service.StoreUserService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/store-user")
+public class StoreUserApiController {
+
+    private final StoreUserConverter storeUserConverter;
+
+    @GetMapping("/me")
+    public StoreUserResponse me(
+            @AuthenticationPrincipal UserSession userSession
+    ){
+        return storeUserConverter.toResponse(userSession);
+    }
+
+}
